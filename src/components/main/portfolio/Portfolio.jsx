d74dc4af5c349fc04e865757e2e64962cdb0d91b
emdee19 <mehmadk05@gmail.com>
2023-03-09 18:04:35 +0500

    creating testimonial section

--- src/components/main/portfolio/Portfolio.jsx
+++ src/components/main/portfolio/Portfolio.jsx
@@ -10,28 +10,28 @@ const Portfolio = () => {
           <h1 className="position-absolute">Portfolio</h1>
           <h2 className="text-center">Portfolio</h2>
           <div className="row mt-5">
-            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6">
-              <img src="./src/assets/images/portfolio-1.jpg" className="img-fluid" alt="portfolio" />
+            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 pe-1">
+              <img src="./src/assets/images/portfolio-1.png" className="img-fluid" alt="portfolio" />
             </div>
 
-            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6">
-              <img src="./src/assets/images/portfolio-2.jpg" className="img-fluid" alt="portfolio" />
+            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 pe-1">
+              <img src="./src/assets/images/portfolio-2.png" className="img-fluid" alt="portfolio" />
             </div>
 
-            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6">
-              <img src="./src/assets/images/portfolio-3.jpg" className="img-fluid" alt="portfolio" />
+            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 pe-1">
+              <img src="./src/assets/images/portfolio-3.png" className="img-fluid" alt="portfolio" />
             </div>
 
-            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 mt-md-2">
-              <img src="./src/assets/images/portfolio-4.jpg" className="img-fluid" alt="portfolio" />
+            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 mt-md-2 pe-1">
+              <img src="./src/assets/images/portfolio-4.png" className="img-fluid" alt="portfolio" />
             </div>
 
-            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 mt-md-2">
-              <img src="./src/assets/images/portfolio-5.jpg" className="img-fluid" alt="portfolio" />
+            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 mt-md-2 pe-1">
+              <img src="./src/assets/images/portfolio-5.png" className="img-fluid" alt="portfolio" />
             </div>
 
-            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 mt-md-2">
-              <img src="./src/assets/images/portfolio-6.jpg" className="img-fluid" alt="portfolio" />
+            <div className="col-lg-2 p-0 col-md-4 col-sm-4 col-6 mt-md-2 pe-1">
+              <img src="./src/assets/images/portfolio-6.png" className="img-fluid" alt="portfolio" />
             </div>
             <div className="text-center portfolio-btn-div">
               <button className="portfolio-btn px-5 py-2">
--- src/components/main/testimonials/Testimonial.jsx
+++ src/components/main/testimonials/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./testimonial.css"
-// import { FaQuoteLeft } from 'react-icons/fa'
+import { FaQuoteLeft } from 'react-icons/fa'
 
 const Testimonial = () => {
   return (
@@ -9,6 +9,22 @@ const Testimonial = () => {
             <div className="container position-relative">
                 <h1 className="position-absolute">Testimonials</h1>
                 <h2>What our clients say</h2>
+                {/* <FaQuoteLeft className="testimonial-icon" /> */}
+                <div className="row">
+                  <div className="col-lg-3 text-end mt-5">
+                    <img src="./src/assets/quote.svg" className="testimonial-icon" alt="" />
+                  </div>
+                  <div className="col-lg-3 text-end mt-5">
+                    <img src="./src/assets/images/testimonial-image.jpg" className="testimonial-image" alt="" />
+                  </div>
+                  <div className="col-lg-6 mt-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque animi atque aut exercitationem quo quis iste culpa sit quasi hic suscipit laborum placeat aliquam cumque, sint iusto sed provident nemo.</p>
+                    <div>
+                      <p><b>Sarah Jordan, CEO</b></p>
+                      <p>Some company</p>
+                    </div>
+                  </div>
+                </div>
             </div>
         </section>
     </>
@@ -19,5 +35,3 @@ export default Testimonial
 
 
 
-
-{/* <FaQuoteLeft /> */}
