44c4176fe0f20193dd22522a5395c9b8e306c41c
emdee19 <mehmadk05@gmail.com>
2023-03-15 11:33:08 +0500

    project completed

--- src/components/main/contact/Contact.jsx
+++ src/components/main/contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
                 <h1 className="position-absolute" id="contact">Contact</h1>
                 <div className="row">
                     <div className="col-lg-6 col-md-5 te">
-                        <h2 className="text-end text-md-center fw-bold">Interested in driving growth?</h2>
+                        <h2 className="text-end text-sm-center fw-bold">Interested in driving growth?</h2>
                         <img src="./src/assets/images/contact-image.png" className="contact-image img-fluid" alt="" />
                     </div>
                     <div className="col-lg-6 col-md-7 p-md-0">
--- src/components/main/creatives/Creative.jsx
+++ src/components/main/creatives/Creative.jsx
@@ -9,7 +9,7 @@ const Creative = () => {
                 <h1 className="position-absolute">Creatives</h1>
                 <h2 className="text-center fw-bold">Our Team</h2>
                 <div className="row pt-5 text-center gx-0">
-                    <div className="col-lg-4 col-md-6 col-sm-6 ps-lg-2">
+                    <div className="col-lg-4 col-md-6 col-sm-6 p-sm-1 ps-lg-2">
                         <img src="./src/assets/images/team-1.jpg" alt="" className="img-fluid" />
                         <p>
                             <b>Muhammad Ismail</b>
@@ -20,7 +20,7 @@ const Creative = () => {
                             Professional Experience: 8 years
                         </p>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 ps-lg-2">
+                    <div className="col-lg-4 col-md-6 col-sm-6 p-sm-1 ps-lg-2">
                         <img src="./src/assets/images/team-2.jpg" alt="" className="img-fluid" />
                         <p>
                             <b>Babar Ali Khan</b>
@@ -31,7 +31,7 @@ const Creative = () => {
                             Professional Experience: 8 years
                         </p>
                     </div>
-                    <div className="col-lg-4 col-md mt-md-5 mt-lg-0 col-sm-6 ps-lg-2">
+                    <div className="col-lg-4 col-md mt-md-5 mt-lg-0 col-sm-12 mt-sm-3 ps-lg-2 text-md-center">
                         <img src="./src/assets/images/team-3.jpg" alt="" className="img-fluid" />
                         <p>
                             <b>Faisal Hayat</b>
