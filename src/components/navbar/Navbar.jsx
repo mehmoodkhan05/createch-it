b298d958998a081c708bc7a905a2e84ce8d3de22
emdee19 <mehmadk05@gmail.com>
2023-03-17 13:14:40 +0500

    adding animations

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,65 +1,87 @@
-import React from 'react'
-import "./navbar.css"
+import { React, useState } from "react";
+import "./navbar.css";
 
 const Navbar = (props) => {
-  return (
-    <>
-        <header className="position-relative navbar-header">
-            <nav className="navbar navbar-expand-lg position-absolute w-100 bg-white fixed-top">
-                <div className="container">
-                    <a className="navbar-brand header-logo" href="#">
-                        <img src="./src/assets/images/CreatechitLogo.png" alt="Logo" />
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#home">
-                                    {props.home}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#about">
-                                    {props.about}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#portfolio">
-                                    {props.portfolio}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#testimonial">
-                                    {props.testimonials}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#contact">
-                                    {props.contact}
-                                </a>
-                            </li>
-                            
-                        </ul>
-                        <button className="header-btn p-1">Need Help?</button>
-                        <span>
-                            <i className="fa fa-search search-icon" aria-hidden="true"></i>
-                        </span>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    </>
-  )
-}
+    const [isSearchOpen, setSearchOpen] = useState(false);
 
-export default Navbar
+    const handleSearchClick = () => {
+        setSearchOpen(!isSearchOpen);
+    };
+
+    const handleInputBlur = () => {
+        setSearchOpen(false);
+    };
+
+    return (
+        <>
+            <header className="navbar-header">
+                <nav className="navbar navbar-expand-lg w-100 bg-white fixed-top">
+                    <div className="container">
+                        <a className="navbar-brand" href="#">
+                            <img src="./src/assets/images/CreatechitLogo.png" className="header-logo" alt="Logo" />
+                        </a>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" aria-current="page" href="#">
+                                        {props.home}
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#about">
+                                        {props.about}
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#portfolio">
+                                        {props.portfolio}
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#testimonial">
+                                        {props.testimonials}
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#contact">
+                                        {props.contact}
+                                    </a>
+                                </li>
+                            </ul>
+                            <button className="header-btn p-1">Need Help?</button>
+                            <div className="search-container">
+                                <span onClick={handleSearchClick}>
+                                    <i
+                                        className="fa fa-search ms-1 mt-1 search-icon"
+                                        aria-hidden="true"
+                                    ></i>
+                                </span>
+                                {isSearchOpen && (
+                                    <input
+                                        className="search-input"
+                                        type="search"
+                                        placeholder="Search..."
+                                        onBlur={handleInputBlur}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default Navbar;
