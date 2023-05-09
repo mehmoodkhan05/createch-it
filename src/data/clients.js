d785cc785630e813e13c901d547f9d1e7ded105f
emdee19 <mehmadk05@gmail.com>
2023-05-09 16:16:28 +0500

    logos Container updated and created a data file for it

---
+++ src/data/clients.js
@@ -0,0 +1,32 @@
+export let clients = [
+    {
+        id: "1",
+        name: "",
+        logo: "/src/assets/logos/wwc-logo.png"
+    },
+    {
+        id: "2",
+        name: "",
+        logo: "/src/assets/logos/proptionary.png"
+    },
+    {
+        id: "3",
+        name: "",
+        logo: "/src/assets/logos/thereyou.png"
+    },
+    {
+        id: "4",
+        name: "",
+        logo: "/src/assets/logos/anne.png"
+    },
+    {
+        id: "5",
+        name: "",
+        logo: "/src/assets/logos/king.png"
+    },
+    {
+        id: "6",
+        name: "",
+        logo: "/src/assets/logos/fiifi.png"
+    },
+]
